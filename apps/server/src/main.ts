a7aae2f1490f2eee39d3f2f709cc950ff8a1c6eb
pononokp <pononokp@unb.ca>
2025-03-19 20:00:38 -0300

    Test

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.setGlobalPrefix('server');
-    app.enableCors();
+    app.enableCors({
+        origin: process.env.FRONTEND_URL,
+    });
     await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap().catch((err) => {
